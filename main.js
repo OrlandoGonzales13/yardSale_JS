9dc9a0503fe02f59bbc71a226b6f5c7f472d3c74
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2022-08-15 20:38:22 -0500

    3. Fusión del menú mobile

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
 
+
+//Function Show desktopMenu 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive'); 
 }
+//Function Show mobile Menu
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
+
