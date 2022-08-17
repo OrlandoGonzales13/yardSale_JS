cd7d19558e8ba1825292587bd8fff832a095f2c0
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2022-08-16 21:29:49 -0500

    5. Agregando productList con JS para imprimirlo en HTML

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarIcon = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 hamburgerMenu.addEventListener('click', toggleMobileMenu);
@@ -46,3 +48,68 @@ function toggleProductDetail(){
     
     productDetail.classList.toggle('inactive');
 }
+
+
+//LIST OF PRODUCTS
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Penny',
+    price: 60,
+    image: 'https://i.pinimg.com/originals/65/95/62/659562e76ba6a3e492ad7ca5f81bc9e5.jpg'
+})
+
+productList.push({
+    name: 'motorcycle',
+    price: 2000,
+    image: 'https://24atv.net/image/cache/catalog/stories/product/tekhnika/bajaj/pulsar-ns-200/bajaj-pulsar-ns-200_red-1500x1000.jpg'
+})
+
+//FUNCTION FOR RENDER PRODUCTS
+function renderProducts(arr){
+    for (product of arr){
+        //VARS LINE FOR LINE HTML
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const div = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const figure = document.createElement('figure');
+        
+        const productImgAdd = document.createElement('img');
+        productDetail.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        //Maketando HTML
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+            productInfo.appendChild(div);
+                div.appendChild(productPrice);
+                div.appendChild(productName);
+                figure.appendChild(productImgAdd);
+    
+        //Print in HTML
+        cardsContainer.appendChild(productCard)
+    }    
+}
+
+renderProducts(productList);
+
+
